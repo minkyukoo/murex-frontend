ed4815f41d37b210fb0724944e52ef5ea1bf3393
Bikramjitdivii <bikramjit@divii.com>
2022-01-07 15:26:50 +0530

    add

--- vue-murex-frontend/layouts/error.vue
+++ vue-murex-frontend/layouts/error.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
+  <div class="error-page">
+    <h1 v-if="error.statusCode === 404">404 Page not found</h1>
     <h1 v-else>An error occurred</h1>
     <NuxtLink to="/">Home page</NuxtLink>
   </div>
@@ -12,3 +12,18 @@
     layout: 'error' // you can set a custom layout for the error page
   }
 </script>
+
+<style lang="scss" scoped>
+.error-page {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(213, 53, 53, 1);
+    text-align: center;
+    padding-top: 200px;
+    font-size: 30px;
+    font-family: sans-serif;
+  }
+</style>
